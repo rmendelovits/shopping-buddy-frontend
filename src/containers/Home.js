7ba8bf48025eed322e19bc22b68435e8fe04f2e3
Reptile <reptitle@outlook.com>
2020-12-27 09:14:02 -0500

    added favicon

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Data Shop Data Buy</h1>
+        <p className="text-muted">Your very own shopping buddy</p>
+      </div>
+    </div>
+  );
+}
