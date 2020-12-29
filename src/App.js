f4bb327bf4274d9a71d0abbe3f65c324c947ceaa
Reptile <reptitle@outlook.com>
2020-12-29 16:39:43 -0500

    added aws amplify to front end

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Routes from "./Routes";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
-          Data Shop Data Buy
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Data Shop Data Buy
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "shopping-buddy-upload",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://h8mdiwgnk1.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_l18pkZ7vX",
+    APP_CLIENT_ID: "36hcr4fprq3mfn1ahh53vrbi0v",
+    IDENTITY_POOL_ID: "us-east-2:53b7d833-e090-4484-aa1e-9566c38c9814",
+  },
+};
+
+export default config;
